48fa542f205c6cefdd8bb6afd37038cb39a0b607
rodrigodojo <rodrigodoj@gmail.com>
2021-01-26 11:31:19 -0300

    exemplo de herança e polimorfismo

---
+++ src/entities/OutsourceEmployee.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class OutsourceEmployee extends Employee {
+	
+	private Double additionalCharge;
+	
+	public OutsourceEmployee() {}
+
+	public OutsourceEmployee(String name, Integer hour, Double valuePerHour, Double additionalCharge) {
+		super(name, hour, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
+
+	public Double getAdditionalCharge() {
+		return additionalCharge;
+	}
+
+	public void setAdditionalCharge(Double additionalCharge) {
+		this.additionalCharge = additionalCharge;
+	}
+	
+	@Override
+	public double payment() {
+		return super.payment() + additionalCharge * 1.1;
+	}
+		
+}
